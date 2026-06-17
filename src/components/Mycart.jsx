23a1d53fa54cb36610d243fccb320aa8c47d5a11
fiati3ewty <fiati3ewty@gmail.com>
2026-06-17 22:03:41 +0700

    Feat: add new My Cart and Order Info display

--- src/components/Mycart.jsx
+++ src/components/Mycart.jsx
@@ -1,29 +1,89 @@
 import React from 'react'
 import Tash from '../assets/tash.png'
+import Ticket from '../assets/ticket.png'
 
 function Mycart() {
   return (
-    <div class='container h-auto mx-auto max-w-4xl md:h-[50px] md:items-center mt-[50px]'>
-        <div class='text-[30px] font-bold mb-[10px]'>My Cart</div>
-        <div class='grid grid-cols-2 grid-rows-auto gap-[20px]'>
-            <div class='rounded-md h-[180px] shadow-sm flex'>
-                <div className="bg-blue-100 h-full w-[10vw] flex items-center justify-center">
+    <div class='container h-auto mx-auto max-w-4xl md:h-12 md:items-center mt-12'>
+        <div class='text-3xl font-bold mb-3'>My Cart</div>
+        <div class='grid grid-cols-2 grid-rows-auto gap-5'>
+
+{/* Google Docs             */}
+            <div class='rounded-md h-45 shadow-sm flex'>
+                <div className="h-full w-[20vw] flex items-center justify-center">
                     <img class='w-7 h-7 border border-[#28dbb6] rounded-full p-1' src={Tash} alt=""></img>
                 </div>
-                <div className="bg-blue-300 h-full w-[45vw]">
-
+                <div className="h-full w-[30vw] flex items-center">
+                    <div className="bg-blue-300 h-4/6 w-full border-none rounded-xl"></div>
                 </div>
-                <div className="bg-blue-500 h-full w-[45vw]">
-                    
+                <div className="h-full w-[50vw]">
+                    <div className="h-full flex flex-col justify-center gap-1 ml-4">
+                        <div class='text-gray-600'>Google Docs</div>
+                        <div className="flex items-center gap-1">
+                            <div class='w-3 h-3 bg-[#28dbb6] rounded-full'></div>
+                            <div class='w-3 h-3 bg-gray-300 rounded-full'></div>
+                            <div class='w-3 h-3 bg-gray-300 rounded-full'></div>
+                            <div>
+                                <select class='block w-full border border-[#28dbb6] text-gray-500 font-semibold text-xs'>
+                                    <option>LV 1-3</option>
+                                    <option>LV 2-3</option>
+                                    <option>LV 3-3</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class='mt-3 text-gray-600'>THB 150</div>
+                    </div>
                 </div>
             </div>
 
-            <div class='rounded-md shadow-sm row-span-2'>
-                
+{/* Order info */}
+            <div class='rounded-md shadow-sm row-span-2 p-7 flex flex-col gap-5'>
+                <div className="w-full font-semibold text-2xl">Order Info</div>
+                <div className=" w-full flex justify-between">
+                    <div class='text-gray-500'>Subtotal</div>
+                    <div class='font-semibold'>THB 300</div>
+                </div>
+                <div className="w-full p0-20 flex items-center gap-3">
+                    <img class='w-7 h-7' src={Ticket} alt="" />
+                    <input class='h-11 w-55 text-sm text-gray-500 pl-3 border border-gray-300' placeholder='Promotion code' type="text" />
+                    <button class='w-30 text-[#28dbb6] text-sm font-bold rounded-xl border-2 border-[#28dbb6] py-2 active:bg-[#28dbb6] active:text-white'>APPLY</button>
+                </div>
+                <hr class='border border-gray-100' />
+                <div className="w-full flex justify-between items-center">
+                    <div class='text-xl'>Total payment</div>
+                    <div class='text-4xl text-[#28dbb6] font-bold trackin-tighter'>THB 300</div>
+                </div>
+                <button class='h-12 bg-[#28dbb6] border border-[#28dbb6] rounded-2xl font-bold tracking-tighter active:bg-white'>LOG IN TO CHECKOUT</button>
             </div>
 
-            <div class='rounded-md shadow-sm h-[180px]'>
-                
+{/* Google Slides */}
+            <div class='rounded-md h-45 shadow-sm flex'>
+                <div className="h-full w-[20vw] flex items-center justify-center">
+                    <img class='w-7 h-7 border border-[#28dbb6] rounded-full p-1' src={Tash} alt=""></img>
+                </div>
+                <div className="h-full w-[30vw] flex items-center">
+                    <div className="bg-orange-300 h-4/6 w-full border-none rounded-xl"></div>
+                </div>
+                <div className="h-full w-[50vw]">
+                    <div className="h-full flex flex-col justify-center gap-1 ml-4">
+                        <div class='text-gray-600'>Google Slides</div>
+                        <div className="flex items-center gap-1">
+                            <div class='w-3 h-3 bg-[#28dbb6] rounded-full'></div>
+                            <div class='w-3 h-3 bg-[#28dbb6] rounded-full'></div>
+                            <div class='w-3 h-3 bg-[#28dbb6] rounded-full'></div>
+                            <div class='w-3 h-3 bg-gray-300 rounded-full'></div>
+                            <div>
+                                <select class='block w-full border border-[#28dbb6] text-gray-500 font-semibold text-xs'>
+                                    <option>LV 1-4</option>
+                                    <option>LV 2-4</option>
+                                    <option>LV 3-4</option>
+                                    <option>LV 4-4</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class='mt-3 text-gray-600'>THB 150</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,28 +6,28 @@ import Cart from '../assets/cart.png'
 function Navbar() {
   return (
     <nav>
-        <div class='container border border-gray-200 h-auto mx-auto max-w-4xl p-2 flex md:flex-row md:justify-around md:h-[50px] md:items-center text-[14px]'>
+        <div class='container border border-gray-200 h-13 mx-auto max-w-4xl p-2 flex flex-row justify-between items-center text-sm'>
             <div>
                 <a href="#">
-                    <img class='w-[150px]' src={Logo} alt="" />
+                    <img class='w-38' src={Logo} alt="" />
                 </a>
             </div>
 
             <ul class='flex font-medium tracking-tight'>
-                <li class='md:mx-2'><a href="#">ABOUT US</a></li>
-                <li class='md:mx-2'><a href="#">PASSCARD</a></li>
-                <li class='md:mx-2'><a href="#">PEOPLE</a></li>
+                <li class='mx-2'><a href="#">ABOUT US</a></li>
+                <li class='mx-2'><a href="#">PASSCARD</a></li>
+                <li class='mx-2'><a href="#">PEOPLE</a></li>
             </ul>
 
             <ul class='flex'>
-                <li class='md:mx-2'><a href="#"><img class='w-[25px]' src={Search} alt="" /></a></li>
-                <div class='relative md:h-[5px]'>
-                        <li class=' md:mx-2'><a href="#"><img class='w-[25px]' src={Cart} alt="" /></a></li>
+                <li class='mx-2'><a href="#"><img class='w-6' src={Search} alt="" /></a></li>
+                <div class='relative h-1'>
+                        <li class=' mx-2'><a href="#"><img class='w-6' src={Cart} alt="" /></a></li>
                         <div class='absolute text-[10px] text-white bg-red-500 h-4 w-4 border-red border rounded-full flex items-center justify-center -bottom-px -right-px'>1</div>
                 </div>
                 <div class='flex flex-row font-medium tracking-tight'>
-                    <li class='md:mx-2'><a href="#">LOGIN</a></li>
-                    <li class='md:mx-2'><a href="#">SIGN UP</a></li>
+                    <li class='mx-4'><a href="#">LOGIN</a></li>
+                    <li class='mx-4'><a href="#">SIGN UP</a></li>
                 </div>
             </ul>
         </div>
