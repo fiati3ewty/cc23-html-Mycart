16dbc91d41ef4a7dfce1dd0b98d781c7c871437b
fiati3ewty <fiati3ewty@gmail.com>
2026-06-17 22:19:28 +0700

    add new docs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -8,5 +9,12 @@ export default defineConfig({
   build: {
     outDir: 'docs',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        Navbar: resolve(__dirname, './src/components/Navbar.jsx'),
+        Mycart: resolve(__dirname, './src/components/Mycart.jsx'),
+      },
+    },
   },
 });
